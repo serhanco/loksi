e388035f5bfafc59b86ec16dce857d9d271cee40
serhanco <60654873+serhanco@users.noreply.github.com>
2021-03-22 14:40:25 +0300

    lokasyonlar.php first commit

---
+++ lokasyonlar.php
@@ -0,0 +1,10 @@
+<?php
+    $ulkeler = array ("Albania","Azerbaijan","Bosnia And Herzegovina", "Bulgaria", "Georgia", "Germany", "Iraq", "Kazakhstan", "Kyrgystan", "Kosovo", "Macedonia", "Moldova", "Montenegro", "Romania", "Russia", "Serbia", "Ukraine", "Uzbekistan");
+    $ofisler = array (
+        "baku" => array("ulke" => "Azerbaijan", "isim" => "at");
+    );
+    
+    foreach ($ulkeler as $c) {
+        echo $c . "</br>";
+    }
+?>
